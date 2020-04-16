fd5500525c9ac4fcf34400daaa3d47afc7d377ae
francoisvaillant <frvaillant@gmail.com>
2020-04-16 11:40:24 +0200

    js styling labels to ajust their size

--- public/assets/js/userForm.js
+++ public/assets/js/userForm.js
@@ -33,11 +33,32 @@ function hidePassword(id) {
 }
 
 /**
- * On loaded DOM content, prepare the fields of the form to check
- * refers to FormControl Class
+
  */
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    /**
+     * Adjust labels sizes to make them equals
+     */
+    $labels = document.getElementsByClassName('input-group-text');
+
+    let maxSize = 0;
+    for (let i=0 ; i<$labels.length ; i++) {
+        if ($labels[i].offsetWidth > maxSize) {
+            maxSize = $labels[i].offsetWidth;
+        }
+    }
+    for (let i=0 ; i<$labels.length ; i++) {
+        $labels[i].style.width = maxSize+'px';
+    }
+    /**
+     *
+     * @type {formControl}
+     * On loaded DOM content, prepare the fields of the form to check
+     * refers to FormControl Class
+     */
+
 
     let controlName = new formControl('lastname', 'name', 'inputGroup-lastname');
     let controlFirstName = new formControl('firstname', 'name', 'inputGroup-firstname');
