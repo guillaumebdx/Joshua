8b69acf75bfdbe7c667f8790111c12feeea48663
francoisvaillant <frvaillant@gmail.com>
2020-04-16 10:36:03 +0200

    feat : ended JS front checker for form

--- public/assets/js/FormControl.js
+++ public/assets/js/FormControl.js
@@ -5,23 +5,51 @@ class formControl {
      * @param elemId The id of the form element you want to check
      * @param type The type of control you want to apply
      */
-    constructor(elemId, type, targetId) {
+    constructor(elemId, type, targetId, compareId='', targetCompareId='') {
         let form = this;
-        this.success_class = 'success';
-        this.error_class   = 'error';
-        this.elem          = document.getElementById(elemId);
-        this.type          = type;
-        this.target        = document.getElementById(targetId);
+        this.success_class      = 'success';
+        this.error_class        = 'error';
+        this.elem               = document.getElementById(elemId);
+        this.type               = type;
+        this.target             = document.getElementById(targetId);
+        this.compareTo          = compareId;
+        this.targetAfterCompare = targetCompareId;
+
         if (this.elem.getAttribute('maxlength')) {
             this.maxLength = this.elem.getAttribute('maxlength');
         }
+        if (this.type === 'password') {
+            this.addPasswordCopy(this.compareTo, this.targetAfterCompare);
+        }
 
         this.elem.addEventListener('change', function(){
-
             if (form.type === 'name') {
                 form.verif_name();
             }
+            if (form.type === 'email') {
+                form.verif_email();
+            }
+            if (form.type === 'text') {
+                form.verif_text();
+            }
+            if (form.type === 'tel') {
+                form.verif_tel();
+            }
+            if (form.type === 'pseudo') {
+                form.verif_pseudo();
+            }
+            if (form.type === 'url') {
+                form.verif_url();
+            }
+            if (form.type === 'password') {
+                form.comparePasswords();
+            }
         });
+    }
+
+    addPasswordCopy (fieldId, targetId) {
+        this.passCopy   = document.getElementById(fieldId);
+        this.targetCopy = document.getElementById(targetId);
     }
 
     verif_name() {
@@ -37,70 +65,70 @@ class formControl {
     verif_text() {
         let regex = /^[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s-]+$/;
         if (this.elem.value === '' || !regex.test(this.elem.value) || this.elem.value.length>this.maxLength) {
-            return false;
+            this.sucessOrError(0);
         } else {
-            return true;
+            this.sucessOrError(1);
         }
     }
 
     verif_tel() {
         let regex=/^[0-9]{10}$/;
         if (this.elem.value === '' || !regex.test(this.elem.value)) {
-            return false;
+            this.sucessOrError(0);
         } else {
-            return true;
+            this.sucessOrError(1);
         }
     }
 
     verif_email() {
         let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (this.elem.value === '' || !regex.test(this.elem.value)) {
-            return false;
+            this.sucessOrError(0);
         } else {
-            return true;
+            this.sucessOrError(1);
         }
     }
 
     verif_pseudo() {
         let regex = /^([0-9a-zA-Z]+[^\s @\-_'"/])$/;
         if (this.elem.value === '' || !regex.test(this.elem.value)) {
-            return false;
+            this.sucessOrError(0);
         } else {
-            return true;
+            this.sucessOrError(1);
         }
     }
 
     verif_url() {
         let regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
         if (!regex.test(this.elem.value) || this.elem.value === '') {
-            return false;
+            this.sucessOrError(0);
         } else {
-            return true;
+            this.sucessOrError(1);
         }
     }
 
     verif_password() {
         let regex = /^(?=.*[0-9])(?=.*[!@#$%^&*/{/}_])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*/{/}_]{8,15}$/;
-        if (!regex.test(this.elem.value)) {
-            return false;
-        } else {
+        if (regex.test(this.elem.value)) {
             return true;
+        } else {
+            return false;
         }
     }
 
-    comparePasswords (pass1, target1, pass2, target2) {
-        if ( pass1.value === pass2.value && verif_password(pass1)) {
-            target1.classList.remove(this.error_class);
-            target1.classList.add(this.success_class);
-            target2.classList.remove(this.error_class);
-            target2.classList.add(this.success_class);
-        } else {
-            target1.classList.add(this.error_class);
-            target1.classList.remove(this.success_class);
-            target2.classList.add(this.error_class);
-            target2.classList.remove(this.success_class);
-        }
+    comparePasswords () {
 
+        if ( this.elem.value === this.passCopy.value && this.verif_password(this.elem)) {
+            this.target.classList.remove(this.error_class);
+            this.target.classList.add(this.success_class);
+            this.targetCopy.classList.remove(this.error_class);
+            this.targetCopy.classList.add(this.success_class);
+        } else {
+            this.target.classList.add(this.error_class);
+            this.target.classList.remove(this.success_class);
+            this.targetCopy.classList.add(this.error_class);
+            this.targetCopy.classList.remove(this.success_class);
+        }
     }
 
     sucessOrError(success) {
--- public/assets/js/user_form.js
+++ public/assets/js/user_form.js
@@ -16,25 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let controlName = new formControl('lastname', 'name', 'inputGroup-lastname');
     let controlFirstName = new formControl('firstname', 'name', 'inputGroup-firstname');
-/*
+    let mail = new formControl('email', 'email', 'inputGroup-email');
+    let pseudo = new formControl('joshua-pseudo', 'pseudo', 'inputGroup-pseudo');
+    let github = new formControl('github', 'pseudo', 'inputGroup-github');
+    let pass1 = new formControl('password', 'password', 'inputGroup-password', 'password-copy', 'inputGroup-password2');
+    let pass2 = new formControl('password-copy', 'password', 'inputGroup-password2', 'password', 'inputGroup-password');
 
-
-    let password1 = document.getElementById('password');
-    let password2 = document.getElementById('password-copy');
-
-    password1.addEventListener('change', function() {
-        pass1 = this;
-        target1 = document.getElementById('inputGroup-password');
-        pass2 = document.getElementById('password-copy');
-        target2 = document.getElementById('inputGroup-password2');
-        comparePasswords (pass1, target1, pass2, target2);
-    });
-    password2.addEventListener('change', function() {
-        pass2 = this;
-        target1 = document.getElementById('inputGroup-password');
-        pass1 = document.getElementById('password');
-        target2 = document.getElementById('inputGroup-password2');
-        comparePasswords (pass1, target1, pass2, target2);
-    });
-*/
 });
