4784015c77111174e9bd17d836735365a86c3ec5
Green <regnault.marien@gmail.com>
2020-04-23 22:34:54 +0200

    Definitively the end of contest creation

--- src/Model/ContestManager.php
+++ src/Model/ContestManager.php
@@ -26,14 +26,14 @@ class ContestManager extends AbstractManager
     public function addContest(object $contest)
     {
         $query     = 'INSERT INTO ' . self::TABLE;
-        $query    .= ' (name, campus, description, duration, image)';
+        $query    .= ' (name, campus_id, description, duration, image)';
         $query    .= ' VALUES (:name, :campus, :description, :duration, :image)';
         $statement = $this->pdo->prepare($query);
         $statement->bindValue(':name', $contest->getProperty('name'), \PDO::PARAM_STR);
         $statement->bindValue(':campus', $contest->getProperty('campus'), \PDO::PARAM_INT);
         $statement->bindValue(':description', $contest->getProperty('description'), \PDO::PARAM_STR);
         $statement->bindValue(':duration', $contest->getProperty('duration'), \PDO::PARAM_INT);
-        $statement->bindValue(':image', $contest->getProperty('url'), \PDO::PARAM_STR);
+        $statement->bindValue(':image', $contest->getProperty('image'), \PDO::PARAM_STR);
 
         if ($statement->execute()) {
             return (int)$this->pdo->lastInsertId();
