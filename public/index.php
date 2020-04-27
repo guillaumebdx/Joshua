15888a7a205e30764493fde964ce1501cc0611e7
Erpeldinger Guillaume <erpeldinger.g@free.fr>
2020-04-27 18:07:27 +0200

    Modif front home

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
+
 if (stristr($_SERVER['REQUEST_URI'], 'admin') != false && (bool)$_SESSION['is_admin'] === false) {
     header('Location: /');
     exit;
 }
+
 $allowedUrl = ['/', '/user/register', '/user/insertuser'];
 if (empty($_SESSION['email']) && !in_array($_SERVER['REQUEST_URI'], $allowedUrl)) {
     header('Location: /');
@@ -18,11 +20,13 @@ if (($test || $test2 || $test3) && $test4) {
     header('Location: /joshua/home');
     exit;
 }
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 if (getenv('ENV') === false) {
     require_once __DIR__ . '/../config/debug.php';
     require_once __DIR__ . '/../config/db.php';
 }
+
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
